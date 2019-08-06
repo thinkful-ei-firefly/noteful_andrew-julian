8a9e6a9c35feb9f2dbcb2b2ffa70e63d11cb3983
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-08-06 10:43:54 -0500

    header work

---
+++ src/main_components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Route, Link} from 'react-router-dom';
+
+function Header() {
+  return (
+    <div className="Header">
+      <header className="App-header">
+            <h1>
+              Noteful
+            </h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
