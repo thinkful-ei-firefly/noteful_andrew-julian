81400dd682a81d4d8b67b404298c6d581ee1c331
Julianb12 <julianb1212@gmail.com>
2019-08-06 14:08:05 -0500

    folders

--- src/folder_components/FolderItem.js
+++ src/folder_components/FolderItem.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { NavLink , Link } from 'react-router-dom'
-import dummy-store from './dummy-store'
+
 
 function FolderItem(props){
   return (
     <li>
-      <Link />
-      {/* <NavLink to={`/folder/${props.id}`}>{props.name}</NavLink> */}
+      
     </li>
   )
 }
