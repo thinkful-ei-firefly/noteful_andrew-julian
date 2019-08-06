f34eadd77c854cd92db635a7a1c2f06a5f65287f
Julianb12 <julianb1212@gmail.com>
2019-08-06 11:27:42 -0500

    folderItems

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './main_components/Header';
+import FolderItem from './folder_components/FolderItem';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <FolderItem />
     </div>
   );
 }
---
+++ src/folder_components/FolderItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import { Link } from 'react-router-dom'
+//mport dummy-store from '../src/dummy-store'
+
+function FolderItem(props){
+  return (
+    <li className='FolderItem'>
+      <h1>hello</h1>
+
+    </li>
+  )
+}
+
+export default FolderItem;
